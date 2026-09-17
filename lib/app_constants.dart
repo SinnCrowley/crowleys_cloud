94306b4d02b19a73512e7c1add83755f777deb2e
crowley <r.negrutsa888@gmail.com>
2026-09-17 22:05:36 +0200

    chore(release): bump version to v0.7.0

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -61,6 +61,6 @@ const documentExtensions = {
 
 const textExtensions = {'.txt', '.md', '.log', '.csv', '.json'};
 
-const String appVersion = '0.6.0';
+const String appVersion = '0.7.0';
 const String githubRepoOwner = 'SinnCrowley';
 const String githubRepoName = 'crowleys_cloud';
